a3ce50f8875514c87b0df3af8de1b38b098b8628
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-27 21:57:58 +0500

    latest

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -11,6 +11,7 @@ import { setUserDetails } from './store/userSlice';
 
 function App() {
   const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
 
   const fetchUserDetails = async()=>{
       const dataResponse = await fetch(SummaryApi.current_user.url,{
@@ -25,16 +26,30 @@ function App() {
       }
   }
 
+  const fetchUserAddToCart = async()=>{
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    setCartProductCount(dataApi?.data?.count)
+  }
 
   useEffect(()=>{
     /**user Details */
     fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
 
   },[])
   return (
     <>
       <Context.Provider value={{
           fetchUserDetails, // user detail fetch 
+          cartProductCount, // current user add to cart product count,
+          fetchUserAddToCart
       }}>
         <ToastContainer 
           position='top-center'
