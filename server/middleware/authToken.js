3c46e663b90d0b52ba40e4a69239cad04d62ff29
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-27 22:45:24 +0500

    neeeee

--- server/middleware/authToken.js
+++ server/middleware/authToken.js
@@ -13,7 +13,7 @@ async function authToken(req,res,next){
             })
         }
 
-        jwt.verify(token, "GDFDSHJCXKGHFDJGHDFJRWEWERWE", function(err, decoded) {
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
             console.log(err)
             console.log("decoded",decoded)
             
