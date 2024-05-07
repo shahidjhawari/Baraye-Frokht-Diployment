c89778be70cc3d711ee8eab0762b0c0976430879
Shahid Iqbal <si656581@gmail.com>
2024-05-07 09:07:14 +0500

    3 dn

--- cleint/src/pages/Login.js
+++ cleint/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import loginIcons from "../assest/signin.gif";
 import { Link, useNavigate } from "react-router-dom";
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 import SummaryApi from "../common";
 import { toast } from "react-toastify";
 import Context from "../context";
@@ -48,9 +49,14 @@ const Login = () => {
     const dataApi = await dataResponse.json();
 
     if (dataApi.success) {
-      localStorage.setItem("token", dataApi.data); 
+      // Use AsyncStorage.setItem instead of localStorage.setItem
+      try {
+        await AsyncStorage.setItem("token", dataApi.data);
+      } catch (error) {
+        console.error("Error saving data to AsyncStorage:", error);
+      }
 
-      toast(dataApi.message, {autoClose: 1500,});
+      toast(dataApi.message, { autoClose: 1500 });
       navigate("/");
       fetchUserDetails();
       fetchUserAddToCart();
@@ -101,16 +107,15 @@ const Login = () => {
                       >
                         {showPassword ? <VisibilityOff /> : <Visibility />}
                       </IconButton>
-                      
                     </InputAdornment>
                   }
                 />
                 <Link
-                to={"/forgot-password"}
-                className="block w-fit ml-auto hover:underline hover:text-amber-500"
-              >
-                Forgot password ?
-              </Link>
+                  to={"/forgot-password"}
+                  className="block w-fit ml-auto hover:underline hover:text-amber-500"
+                >
+                  Forgot password ?
+                </Link>
               </FormControl>
             </div>
 
--- server/controller/user/userSignIn.js
+++ server/controller/user/userSignIn.js
@@ -1,66 +1,56 @@
-const bcrypt = require('bcryptjs')
-const userModel = require('../../models/userModel')
+const bcrypt = require('bcryptjs');
+const userModel = require('../../models/userModel');
 const jwt = require('jsonwebtoken');
+const AsyncStorage = require('@react-native-async-storage/async-storage');
 
-async function userSignInController(req,res){
-    try{
-        const { email , password} = req.body
+async function userSignInController(req, res) {
+    try {
+        const { email, password } = req.body;
 
-        if(!email){
-            throw new Error("Please provide email")
+        if (!email) {
+            throw new Error("Please provide email");
         }
-        if(!password){
-             throw new Error("Please provide password")
-        }
-
-        const user = await userModel.findOne({email})
-
-       if(!user){
-            throw new Error("User not found")
-       }
-
-       const checkPassword = await bcrypt.compare(password,user.password)
-
-       console.log("checkPassoword",checkPassword)
-
-       if(checkPassword){
-        const tokenData = {
-            _id : user._id,
-            email : user.email,
-        }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 24 * 30 }); // 30 days in seconds
-
-        const tokenOption = {
-            httpOnly : true,
-            secure : true,
-            sameSite: 'None'
+        if (!password) {
+            throw new Error("Please provide password");
         }
 
-        res.cookie("token",token,tokenOption).status(200).json({
-            message : "Login successfully",
-            data : token,
-            success : true,
-            error : false
-        })
+        const user = await userModel.findOne({ email });
 
-       }else{
-         throw new Error("Please check Password")
-       }
+        if (!user) {
+            throw new Error("User not found");
+        }
 
+        const checkPassword = await bcrypt.compare(password, user.password);
 
+        console.log("checkPassword", checkPassword);
 
+        if (checkPassword) {
+            const tokenData = {
+                _id: user._id,
+                email: user.email,
+            };
 
+            const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 24 * 30 }); // 30 days in seconds
 
+            // Store token in AsyncStorage
+            await AsyncStorage.setItem("token", token);
 
-
-    }catch(err){
+            res.status(200).json({
+                message: "Login successfully",
+                data: token,
+                success: true,
+                error: false
+            });
+        } else {
+            throw new Error("Please check Password");
+        }
+    } catch (err) {
         res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
     }
-
 }
 
-module.exports = userSignInController
+module.exports = userSignInController;
