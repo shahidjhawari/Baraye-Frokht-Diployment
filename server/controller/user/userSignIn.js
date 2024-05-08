ae9c12a332c64318ec75529531f33d5458f3e972
Shahid Iqbal <si656581@gmail.com>
2024-05-08 09:24:55 +0500

    cookie opt remv

--- server/controller/user/userSignIn.js
+++ server/controller/user/userSignIn.js
@@ -30,11 +30,7 @@ async function userSignInController(req,res){
         }
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 24 * 30 }); // 30 days in seconds
 
-        const tokenOption = {
-            httpOnly : true,
-            secure : true,
-            sameSite: 'None'
-        }
+
 
         res.cookie("token",token,tokenOption).status(200).json({
             message : "Login successfully",
