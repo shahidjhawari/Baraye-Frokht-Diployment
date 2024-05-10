cf37053b7c1f52d6f12878c9cc9ad720191e411e
Shahid Iqbal <si656581@gmail.com>
2024-05-10 15:42:54 +0500

    https done

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -21,6 +21,21 @@ const productSchema = mongoose.Schema(
   }
 );
 
+// Pre-save middleware to ensure product image URLs are using HTTPS
+productSchema.pre('save', function(next) {
+  // Iterate through each product image URL
+  this.productImage = this.productImage.map(imageUrl => {
+    // If the URL doesn't already start with HTTPS, prepend it
+    if (!imageUrl.startsWith('https://')) {
+      // Assuming the URL starts with HTTP, replace it with HTTPS
+      return imageUrl.replace(/^http:\/\//i, 'https://');
+    }
+    return imageUrl;
+  });
+  // Proceed to save
+  next();
+});
+
 const productModel = mongoose.model("product", productSchema);
 
 module.exports = productModel;
