50e86354f295a880f58e235f7a5dab85625e3732
Shahid Iqbal <si656581@gmail.com>
2024-05-10 18:42:04 +0500

    banner change

--- cleint/src/components/BannerProduct.js
+++ cleint/src/components/BannerProduct.js
@@ -5,11 +5,11 @@ import image2 from '../assest/banner/img2.webp';
 import image3 from '../assest/banner/img3.jpg';
 import image4 from '../assest/banner/img4.jpg';
 import image5 from '../assest/banner/img5.webp';
-import image1Mobile from '../assest/banner/img1_mobile.jpg';
-import image2Mobile from '../assest/banner/img2_mobile.webp';
-import image3Mobile from '../assest/banner/img3_mobile.jpg';
-import image4Mobile from '../assest/banner/img4_mobile.jpg';
-import image5Mobile from '../assest/banner/img5_mobile.png';
+import image1Mobile from '../assest/banner/1.jpg';
+import image2Mobile from '../assest/banner/1.webp';
+import image3Mobile from '../assest/banner/2.webp';
+import image4Mobile from '../assest/banner/3.webp';
+import image5Mobile from '../assest/banner/4.webp';
 import banner1 from "../assest/banner1.png"
 
 const BannerProduct = () => {
